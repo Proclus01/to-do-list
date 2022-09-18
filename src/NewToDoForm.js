3b859219996549e14179f5075a760540178f99b5
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-17 21:29:57 -0700

    plan out newtodoform component with documentation

--- src/NewToDoForm.js
+++ src/NewToDoForm.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 class NewToDoForm extends Component {
 
+    // onSubmit will create a new ToDo
+    //  - call a function passed as props from parent
+
+    // Render a form with one text input
+
     render() {
         return (
             <div>
