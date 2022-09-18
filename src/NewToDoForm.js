846fb864f1e021e1e0498037f7e10ac1f65607d9
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-17 21:20:50 -0700

    begin work on new todoform component

---
+++ src/NewToDoForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NewToDoForm extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default NewToDoForm;
