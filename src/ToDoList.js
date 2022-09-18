a6ccf213145a9389d61e2ec128a7718f784d5fdd
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-17 21:25:49 -0700

    plan out todolist component with documentation

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import ToDo from './ToDo.js';
 
 class ToDoList extends Component {
 
+    // Has state that contains all the ToDos
+    constructor(props) {
+        this.state = [
+            {
+                // We need an id, and a value
+            }
+        ]
+    }
+
+    // Render all the ToDo components from state
     render() {
         return (
             <div>
-
+                <p>I will be the main component</p>
             </div>
         )
     }
