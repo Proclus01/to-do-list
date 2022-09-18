81b264e83d31996c04e7208752f32736d5f80cdc
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-17 21:40:06 -0700

    add test object to state for render

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import ToDo from './ToDo.js';
 import NewToDoForm from './ToDo.js';
+import { v4 as uuidv4 } from 'uuid';
 
 class ToDoList extends Component {
 
     // Has state that contains all the ToDos
     constructor(props) {
         super(props);
-        this.state = [
-            {
-                // We need an id, and a value
-            }
-        ]
+        this.state = {
+            todos: [
+                // Array of objects containing id and value
+                {id: uuidv4(), value: "I'm a to-do item!"}
+            ]
+        }
     }
 
     // Render all the ToDo components from state
