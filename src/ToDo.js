3d1f09c2dcd13c7c7ec7c776425270a86f06e0b0
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-17 21:28:50 -0700

    plan out todo component with documentation

--- src/ToDo.js
+++ src/ToDo.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 class ToDo extends Component {
 
+    // Display a div with the task of the todo
+
+    // Has an X to delete the ToDo
+    //  - will call a function passed as props from parent
+
     render() {
         return (
             <div>
