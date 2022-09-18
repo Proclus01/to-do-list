679916f3c42b6b3c99869f8bddf2d1b90f1277b3
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-17 21:19:40 -0700

    begin work on todolist component

---
+++ src/ToDoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ToDoList extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ToDoList;
